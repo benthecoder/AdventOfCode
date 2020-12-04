a74d10d8bb263f5b6ac18ae5bd1099ac49502d64
benthecoder <benthecoder07@gmail.com>
2020-12-04 16:26:11 +0800

    updated code for day 2

--- Day_2/solution.py
+++ Day_2/solution.py
@@ -1,3 +1,4 @@
+# read txt file and return necessary elements
 def read(line):
 	string = line.rstrip().replace(':', '').split(' ')
 	num_1, num_2 = map(int, string[0].split('-'))
@@ -5,18 +6,21 @@ def read(line):
 	pw = string[2]
 	return num_1, num_2, letter, pw
 
+# Part 1 solution
 def sol_1(line):
 	min, max, letter, pw = read(line)
 	if min <= pw.count(letter) <= max:
 		return 1
 	return 0
 
+# Part 2 solution
 def sol_2(line):
 	pos_1, pos_2, letter, pw = read(line)
 	if (pw[pos_1 - 1] == letter) != (pw[pos_2 - 1] == letter):
 		return 1
 	return 0
 
+# returns count for part 1 and 2
 def counts():
 	count_1, count_2 = 0, 0
 	with open("input.txt", "r") as f:
